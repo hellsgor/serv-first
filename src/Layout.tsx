05ba05471c84a1a3de5e6d9c7f9801a1c2fe270e
Igor Storchevoy <hellsgor@gmail.com>
2026-02-22 20:35:58 +0000

    add routing

---
+++ src/Layout.tsx
@@ -0,0 +1,23 @@
+import type { ReactNode } from 'react';
+import { ROUTES } from './config/routes';
+import { Link, Outlet } from 'react-router';
+
+export default function Layout() {
+  return (
+    <>
+    <header>
+      <nav>
+        <ul>
+          {Object.entries(ROUTES).map(([key, value]) => 
+            <li key={key}>
+              <Link to={value}>{key}</Link>
+            </li>)}
+        </ul>
+      </nav>
+    </header>
+    <main>
+      <Outlet/>
+    </main>
+    </>
+  )
+}
